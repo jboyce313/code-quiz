757548df99c6db7c3fd9344389abe80a9cefe767
Jacob Boyce <jboyce313@gmail.com>
2023-01-02 10:14:14 -0600

    added functionality to hide previous questions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var startButton = document.querySelector(".start-btn");
 var quizArea = document.querySelector(".quiz-area");
+var questionArea = document.querySelector(".question-area");
 
 // questions as objects
 var q1 = {
@@ -26,32 +27,47 @@ questionNumber = 0;
 
 // grabs question from question array and creates HTML to display it
 var generateQuestion = function () {
-  if (questionNumber === 0) startButton.classList.add("hidden");
+  // checks if at end of questions array
+  if (questionNumber >= questions.length) return;
+
+  // checks to see if previous question displayed and deletes it
+  // if no question displayed, displays first question
+  if (questionArea.classList.contains("hidden")) {
+    questionArea.classList.remove("hidden");
+    startButton.classList.add("hidden");
+  } else {
+    document.querySelector(".question").remove();
+    var answers = document.querySelectorAll(".answer-btn");
+    for (var i = 0; i < answers.length; i++) {
+      answers[i].remove();
+    }
+  }
 
   // create HTML for question and answers
   var question = document.createElement("h2");
+  question.classList.add("question");
   question.textContent = questions[questionNumber].question;
-  quizArea.appendChild(question);
+  questionArea.appendChild(question);
 
   var answer1 = document.createElement("button");
   answer1.classList.add("answer-btn");
   answer1.textContent = "1. " + questions[questionNumber].a1;
-  quizArea.appendChild(answer1);
+  questionArea.appendChild(answer1);
 
   var answer2 = document.createElement("button");
   answer2.classList.add("answer-btn");
   answer2.textContent = "2. " + questions[questionNumber].a2;
-  quizArea.appendChild(answer2);
+  questionArea.appendChild(answer2);
 
   var answer3 = document.createElement("button");
   answer3.classList.add("answer-btn");
   answer3.textContent = "3. " + questions[questionNumber].a3;
-  quizArea.appendChild(answer3);
+  questionArea.appendChild(answer3);
 
   var answer4 = document.createElement("button");
   answer4.classList.add("answer-btn");
   answer4.textContent = "4. " + questions[questionNumber].a4;
-  quizArea.appendChild(answer4);
+  questionArea.appendChild(answer4);
 
   // checks for correct answer upon button click and moves to next question
   questionNumber++;
